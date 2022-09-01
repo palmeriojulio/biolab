4bf7078353cf403f21eaa5028bb31e94bd412164
Palmério <palmerio_julio@hotmail.com>
2022-08-31 23:32:45 -0300

    teste

---
+++ biolab-backend/src/main/java/br/com/pcode/biolabbackend/BiolabApplication.java
@@ -0,0 +1,13 @@
+package br.com.pcode.biolabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiolabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiolabApplication.class, args);
+	}
+
+}
---
+++ biolab-backend/src/test/java/br/com/pcode/biolabbackend/BiolabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pcode.biolabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
