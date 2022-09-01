8bc121d496f421ed430a9e486d0f3d7b98263672
Palmério <palmerio_julio@hotmail.com>
2022-09-01 15:19:28 -0300

    Create Pessoa.java
    Criando classe da entidade pessoa, usando anotações do Spring JPA e Lombok.

---
+++ biolab-backend/src/test/java/br/com/pjcode/biolab/domain/Pessoa.java
@@ -0,0 +1,51 @@
+package br.com.pjcode.biolab.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pessoa")
+@Getter @Setter
+public class Pessoa implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Column(name = "rg")
+	private String rg;
+	
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	@Column(name = "diabetico")
+	private String diabetico;		
+	
+	@Column(name = "medicamentos_que_toma")
+	private String medicamentosQueToma;
+	
+	@Column(name = "outras_informacoes")
+	private String outrasInformacoes;
+	
+}
