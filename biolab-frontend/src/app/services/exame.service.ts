9e7c28911e6cac3883832c2d1717d2fc686387d4
Palmério <palmerio_julio@hotmail.com>
2022-09-22 23:55:01 -0300

    Criando mensagem de erro para o método deletar.

--- biolab-frontend/src/app/services/exame.service.ts
+++ biolab-frontend/src/app/services/exame.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, throwError } from 'rxjs';
 
 import { API } from './../../../app.api';
 import { Exame } from './../models/exame-model';
@@ -12,7 +13,7 @@ export class ExameService {
 
   localUrl: string;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
     this.localUrl = `${API}biolab/`;
   }
 
@@ -21,15 +22,26 @@ export class ExameService {
   }
 
   salvarExame(exame: Exame) {
-    console.log(exame)
-    return this.http.post(`${this.localUrl}exame`, JSON.stringify(exame));
+    return this.http.post(`${this.localUrl}exame`, exame);
   }
 
   atualizarExame(exame: Exame) {
-    return this.http.put(`${this.localUrl}exame`, JSON.stringify(exame));
+    return this.http.put(`${this.localUrl}exame`, exame);
   }
 
   deletarExame(id: number) {
     return this.http.delete(`${this.localUrl}exame/${id}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T): () => Observable<T> {
+    return (): Observable<T> => {
+      const toastMessage = 'Erro ao ' + operation + '.';
+      this.snackBar.open(toastMessage, 'X');
+
+      if (!result) {
+        return throwError(new Error());
+      }
+      return of(result as T);
+    };
   }
 }
