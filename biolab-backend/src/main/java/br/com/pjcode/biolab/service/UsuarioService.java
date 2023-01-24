8cc9410a556ade6ba68906d9e475c09c1d104cea
Palmério <palmerio_julio@hotmail.com>
2023-01-23 22:14:54 -0300

    Criando serviço de usuario

---
+++ biolab-backend/src/main/java/br/com/pjcode/biolab/service/UsuarioService.java
@@ -0,0 +1,59 @@
+package br.com.pjcode.biolab.service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import br.com.pjcode.biolab.dao.UsuarioRepository;
+import br.com.pjcode.biolab.domain.Usuario;
+import br.com.pjcode.biolab.dto.UsuarioDto;
+
+@Service
+public class UsuarioService {
+
+	final UsuarioRepository usuarioRepository;
+
+	public UsuarioService(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+	
+	public List<UsuarioDto> findAll() {
+		try {
+			return usuarioRepository.findAll().stream()
+					.map(u -> UsuarioDto.fromUsuario(u))
+					.sorted((u1, u2)-> u1.getId().compareTo(u2.getId()))
+					.collect(Collectors.toList());
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	public UsuarioDto save(UsuarioDto dto) {
+		try {
+			var usuario = usuarioRepository.save(UsuarioDto.toUsuario(dto));			
+			return convertReturn(usuario);
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private UsuarioDto convertReturn(Usuario usuario) {
+		try {
+			if(Objects.nonNull(usuario)) {
+				return UsuarioDto.fromUsuario(usuario);
+			} else {
+				return null;			
+			}			
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+}
